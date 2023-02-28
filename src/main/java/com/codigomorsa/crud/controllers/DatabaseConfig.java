a4b119bd81b02229e9e850d29a85c5b6eda6c6b6
Martin Cornejo <mart256@gmail.com>
2023-02-28 23:37:28 +0100

    Added DatabaseConfig

--- src/main/java/com/codigomorsa/crud/controllers/DatabaseConfig.java
+++ src/main/java/com/codigomorsa/crud/controllers/DatabaseConfig.java
@@ -1,18 +1,35 @@
 package com.codigomorsa.crud.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
 
+    @Value("${datasource.customs.query-timeout}")
+    private int queryTimeout;
+
     @Bean
     @ConfigurationProperties(prefix = "datasource.my-connection")
     public DataSource crudDataSource() {
         return DataSourceBuilder.create().build();
+    }
+    @Bean
+    public JdbcTemplate crudJdbcTemplate(DataSource crudDataSource) {
+        var jdbcTemplate = new JdbcTemplate(crudDataSource);
+        jdbcTemplate.setQueryTimeout(queryTimeout);
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate crudNamedParameterJdbcTemplate(JdbcTemplate crudJdbcTemplate) {
+        return new NamedParameterJdbcTemplate(crudJdbcTemplate);
     }
 }
